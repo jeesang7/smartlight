d5b58055a679e90628ab77811df87f06bd3efeb5
Jeesang Kim <jeenowden@gmail.com>
2021-08-22 00:20:15 +0900

    create project with .gitignore

---
+++ SmartLight/Scene1Page.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+
+namespace SmartLight
+{
+    public partial class Scene1Page : View
+    {
+        public Scene1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
