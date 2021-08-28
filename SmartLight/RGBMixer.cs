6a8c79cc622a9b6ba02eea7c5bf4f6fdb7bcbf14
Jeesang Kim <jeenowden@gmail.com>
2021-08-29 05:59:11 +0900

    add RGBMixer control for the slider

---
+++ SmartLight/RGBMixer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using Tizen.Peripheral.Gpio;
+
+
+namespace SmartLight
+{
+    class RGBMixer
+    {
+        GpioPin gpioPwm;
+        int dutyCycle = 0;
+        Thread thread;
+
+        public RGBMixer(int pinnum)
+        {
+            gpioPwm = new GpioPin(pinnum, GpioPinDriveMode.OutputInitiallyHigh);
+            gpioPwm.Write(GpioPinValue.High);
+            thread = new Thread(new ThreadStart(Run));
+        }
+
+        public void AnalogWrite(int duty)
+        {
+            dutyCycle = duty;
+
+            if (!thread.IsAlive)
+                thread.Start();
+        }
+
+        void Run()
+        {
+            Console.WriteLine("Thread#{0}: Begin", Thread.CurrentThread.ManagedThreadId);
+
+            while (true)
+            {
+                // 50Hz
+                gpioPwm.Write(GpioPinValue.Low);
+                Thread.Sleep(dutyCycle / 5);
+                gpioPwm.Write(GpioPinValue.High);
+                Thread.Sleep((100 - dutyCycle) / 5);
+            }
+        }
+    }
+}
--- SmartLight/Scene1Page.xaml.cs
+++ SmartLight/Scene1Page.xaml.cs
@@ -1,12 +1,19 @@
-﻿using Tizen.NUI.BaseComponents;
+﻿using System;
+using Tizen.NUI.BaseComponents;
 using Tizen.Peripheral.Gpio;
+
 
 namespace SmartLight
 {
     public partial class Scene1Page : View
     {
         private const int SENSOR_LED_GPIO_NUMBER = 24;
+        private const int RGBLED_R_GPIO_NUMBER = 17;
+        private const int RGBLED_B_GPIO_NUMBER = 27;
+        private const int RGBLED_G_GPIO_NUMBER = 22;
+
         private GpioPin ledGpio;
+        private RGBMixer ledPwmR, ledPwmB, ledPwmG;
         private RESTClient restClient;
 
         public Scene1Page()
@@ -14,6 +21,9 @@ namespace SmartLight
             InitializeComponent();
 
             ledGpio = new GpioPin(SENSOR_LED_GPIO_NUMBER, GpioPinDriveMode.OutputInitiallyLow);
+            ledPwmR = new RGBMixer(RGBLED_R_GPIO_NUMBER);
+            ledPwmB = new RGBMixer(RGBLED_B_GPIO_NUMBER);
+            ledPwmG = new RGBMixer(RGBLED_G_GPIO_NUMBER);
             restClient = new RESTClient();
             restClient.Read();
         }
@@ -35,6 +45,27 @@ namespace SmartLight
         public void Stop()
         {
             ledGpio.Dispose();
+        }
+
+        private void OnRValueChanged(object sender, Tizen.NUI.Components.SliderValueChangedEventArgs e)
+        {
+            Console.WriteLine("R value" + e.CurrentValue);
+
+            ledPwmR.AnalogWrite((int)e.CurrentValue);
+        }
+
+        private void OnGValueChanged(object sender, Tizen.NUI.Components.SliderValueChangedEventArgs e)
+        {
+            Console.WriteLine("G value" + e.CurrentValue);
+
+            ledPwmG.AnalogWrite((int)e.CurrentValue);
+        }
+
+        private void OnBValueChanged(object sender, Tizen.NUI.Components.SliderValueChangedEventArgs e)
+        {
+            Console.WriteLine("B value" + e.CurrentValue);
+
+            ledPwmB.AnalogWrite((int)e.CurrentValue);
         }
     }
 }
